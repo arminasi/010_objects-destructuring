d5be41a307fd70a441df626472d5fb660cc0cacc
arminasi <armanminasyanc@gmail.com>
2021-12-15 01:13:20 +0400

    feat: get info using destructuring

---
+++ 1_get_info_desstruct/get_using_destruc.js
@@ -0,0 +1,9 @@
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+
+name = person.name;
+country = "Nigeria";
+job = person.job;
+
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
